459ba9945880a55a9c82c01b7582a02db74daf93
Yeabsira ashenafi <ashenafiyeabsira@gmail.com>
2020-09-04 03:09:54 +0300

    few changes

---
+++ pages/users.vue
@@ -0,0 +1,24 @@
+<template>
+  <span>
+    <v-text-field v-model="id" label="Textfield" solo></v-text-field>
+    <v-btn @click="goId()">Submit</v-btn>
+    <nuxt-child />
+  </span>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      id: '',
+    }
+  },
+  methods: {
+    goId() {
+      this.$router.push('/users/' + this.id)
+    },
+  },
+}
+</script>
+
+<style></style>
---
+++ pages/users/_name/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <span>hey</span>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
